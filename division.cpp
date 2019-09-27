32761b62738ebafd3083447678c48cf2e895d03e
Jueun-Park <elpion19@gmail.com>
2019-09-27 16:27:49 +0900

    first cpp

---
+++ division.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << setprecision(16);
+    double a, b;
+    cin >> a >> b;
+    cout << a / b;
+    return 0;
+}
